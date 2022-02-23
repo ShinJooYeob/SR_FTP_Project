7a99b06f4cc3a650d68c5d1c61541d79cd24b065
DESKTOP-H89IHKA\peh96 <peh960725@naver.com>
2022-02-24 02:02:50 +0900

    별 다른 수정사항 없음

--- TeamPortfolio/Client/private/Object_OrbitCube.cpp
+++ TeamPortfolio/Client/private/Object_OrbitCube.cpp
@@ -65,32 +65,33 @@ _int CObject_OrbitCube::Update(_float fTimeDelta)
 
 
 
-	if (!m_bIsTurning && GetSingle(CGameInstance)->Get_DIKeyState(DIK_K) & DIS_Down)
+	if (!m_bIsTurning && GetSingle(CGameInstance)->Get_DIKeyState(DIK_K) & DIS_Down)//이건 X와 Z좌표로 위치를 구하고 어떤 각도로 움직일 것인지를 정하고 있다.
 	{
 
 		//일단 트랜스폼의 위치를 가져옴
 		_float3 vTempObjectPos = m_ComTransform->Get_MatrixState(CTransform::STATE_POS);
 
-		//그리고 로테이션 축을 가져온다. 이때 우린 Y축이 필요없다. x와 z만 가져와서 공전 위치를 정해줄테니깐.
+		//그리고 로테이션 축을 가져온다. 이때 우린 Y축이 필요없다. x와 z만 가져와서 공전 위치를 정해줄테니깐.  추가내용)아직까진 X,Y,Z가 들어있다.
 		_float3 vRotAxis = m_RotAxis;
 
-		//혹시 모르니 예외처리로 Y에 0을 넣어준다.
+		//그렇기 때문에 y를 0으로 초기화시켜준다.
 		vRotAxis.y = 0;
 
-		//로테이션의 룩을 구해준다.
+		//로테이션의 룩을 구해준다.    오브젝트포지션 - 임의의 축을 해서 임의의 축이 오브젝트로 향한 룩 벡터가 나왔다.
 		_float3 vTempLookVector_Y_Zero = vTempObjectPos - vRotAxis;
 
-		//vTempObjectPos의 y값이 들어왔으니 0으로 다시 맞춰준다.
+		//vTempObjectPos의 y값이 들어왔으니 0으로 다시 맞춰준다.           추가내용)Y가 사라지고  X,0,Z가 들어있다.
 		vTempLookVector_Y_Zero.y = 0;
 
-		//아크코사인은 각도를 알 수 있다. 일단 이건 집에가서 보자.
-		_float fRadianAngle = acosf(vTempLookVector_Y_Zero.Get_Nomalize().Get_Dot(_float3(1, 0, 0)));
+		//아크코사인은 각도를 알 수 있다. 일단 이건 집에가서 보자. 기준이 필요하기 때문이다. 모든 객체가 정면을 향하고 깨끗하게 90도라고 말할 순 없다. 그러기 때문에 x축을 기준으로 사이각을 구한거다.
+		_float fRadianAngle = acosf(vTempLookVector_Y_Zero.Get_Nomalize().Get_Dot(_float3(1, 0, 0))); //x로 향하는 방향벡터와 우리가 구한 좌표값을 내적하여 아크코사인으로 그 사이각을 구한다!!!
 
 		if (vRotAxis.z > vTempObjectPos.z)
 			//라디안으로 계산을 하다가 180도를 넘어가면 코사인에 문제가 생긴다. 그래서 예외처리로 360도에서 라디안 각만큼 뺀 값을 이용한다.
 			fRadianAngle = 2 * D3DX_PI - fRadianAngle;
 		//
 
+		//라디안을 구했으니 스타트 앵글로 맞춰준다.
 		//스타트 앵글을 다시 맞춰준다.
 		m_StartAngle = fRadianAngle;
 
@@ -108,7 +109,7 @@ _int CObject_OrbitCube::Update(_float fTimeDelta)
 		m_bIsTurning = true;
 
 	}
-	if (m_bIsTurning)//여기에 있는건 Y값을 구하기 위함이다. 왜냐하면 위에선 y를 0으로 만들고 새로 x,z로 좌표를 구했기 때문이다.
+	if (m_bIsTurning)//여기에 있는건 Y값을 구하기 위함이다. 왜냐하면 위에선 y를 0으로 만들고 새로 x,z로 좌표를 구하고 각도를 구했기 때문이다.
 	{
 		//보간하려고 값 증가시킴
 		m_PassedTime += fTimeDelta;
@@ -117,16 +118,17 @@ _int CObject_OrbitCube::Update(_float fTimeDelta)
 		_float3 vTempObjectPos = m_ComTransform->Get_MatrixState(CTransform::STATE_POS);
 		vTempObjectPos.y = 0;
 
-		//이게 그 새로 구한 좌표에 들어갈 예정
+		//이게 그 새로 구한 좌표에 들어갈 예정 즉, 임의의 축
 		_float3 vRevolutoinPos_Y_Zero = m_RotAxis;
 		vRevolutoinPos_Y_Zero.y = 0;
 
+		//일단 새로 구한 임의의 축의 룩 벡터를 구하고 y에 0를 넣는다. 이유: 어차피 X와 Z로 좌표만 설정해주려고 즉, y방향으로 돌기 때문에
 		_float3 vTempLookVector_Y_Zero = vTempObjectPos - vRevolutoinPos_Y_Zero;
 		vTempLookVector_Y_Zero.y = 0;
 
-		_float fDist = vTempLookVector_Y_Zero.Get_Distance(_float3(0, 0, 0));
+		_float fDist = vTempLookVector_Y_Zero.Get_Distance(_float3(0, 0, 0));//거리를 구하기 위함이다. 0,0,0을 하는 이유는 vTempLookVector_Y_Zero의 순수하게 그 거리만을 알기 위해서이다.
 
-		_float fRadianAngle = GetSingle(CGameInstance)->Easing(TYPE_ElasticIn, m_StartAngle, m_TargetAngle, m_PassedTime);
+		_float fRadianAngle = GetSingle(CGameInstance)->Easing(TYPE_ElasticIn, m_StartAngle, m_TargetAngle, m_PassedTime); //보간처리해주고
 
 		if (m_PassedTime > 1.f)
 		{
@@ -135,15 +137,19 @@ _int CObject_OrbitCube::Update(_float fTimeDelta)
 			fRadianAngle = m_TargetAngle;
 		}
 
-
+		//cosf으로 x좌표를 sinf으로 z좌표를 알 수 있으며 거기에 거리를 곱하면 그 좌표의 주솟값을 얻을 수 있다. 
 		vTempObjectPos.x = cosf(fRadianAngle) * fDist;
 		vTempObjectPos.z = sinf(fRadianAngle) * fDist;
 		
-		vTempObjectPos += vRevolutoinPos_Y_Zero;
+		vTempObjectPos += vRevolutoinPos_Y_Zero; //새로 구한 임의의 축 x와 z를 넣어줘야 우리가 생각했던 임의의 축으로 이동한다.
 
-		vTempObjectPos.y = m_ComTransform->Get_MatrixState(CTransform::STATE_POS).y;
+		//우린 y가 0이였으므로 포지션중에서 y만 가져와서 넣어준다.
+		vTempObjectPos.y = m_ComTransform->Get_MatrixState(CTransform::STATE_POS).y; //우린 vTempObjectPos.y를 0으로 해줬으므로 Y값을 구해 가져온다.
+																					//우린 Y 축 기준으로 x와z만 이동시켜줬으므로 Y는 처음 넣어줬던 값 그대로이다.
+																					//만약 X축 또는 Z축 기준으로 하고자 한다면 그걸 기준으로 해주면 된다.
 
-		m_ComTransform->Set_MatrixState(CTransform::STATE_POS, vTempObjectPos);
+		//그리고 템프 오프젝트를 셋 매트릭스로 보내준다.
+		m_ComTransform->Set_MatrixState(CTransform::STATE_POS, vTempObjectPos); //그렇게 구한 값을 넣어준다.
 
 		//m_pTransform->LookAt(vRevPos);
 
