310b7fc7eaac204cf2d0d409575f60f74e4f7bfc
Jiwhan <>
2022-03-02 00:47:56 +0900

    포탈 / 에스컬레이터 생성확인

--- TeamPortfolio/Client/private/Scene_IMGUI.cpp
+++ TeamPortfolio/Client/private/Scene_IMGUI.cpp
@@ -4,6 +4,7 @@
 #include "Player.h"
 #include "MapLoadMgr.h"
 #include "Object_PortalCube_A.h"
+#include "Object_EscalatorCube.h"
 
 CScene_IMGUI::CScene_IMGUI(LPDIRECT3DDEVICE9 GraphicDevice)
 	:CScene(GraphicDevice)
@@ -131,10 +132,16 @@ HRESULT CScene_IMGUI::Ready_Layer_SkyBox(const _tchar * pLayerTag)
 HRESULT CScene_IMGUI::Ready_Layer_Terrain(list<SPECIALCUBE*>* listdata)
 {
 	CObject_PortalCube_A::POTALDESC potalDesc;
+	CObject_EscalatorCube::ESCALATORDESC escalDesc;
+
 
 	potalDesc.iNowScene = SCENEID::SCENE_IMGUISCENE;
 	potalDesc.vPos_A_Cube = _float3(0, 0 ,0);
 	potalDesc.vPos_B_Cube = _float3(0, 0, 0);
+
+	escalDesc.vStartPos = _float3(0, 0, 0);
+	escalDesc.vEndPos = _float3(0, 0, 0);
+
 
 	int count_Potal = 0;
 	int count_Escalator = 0;
@@ -147,14 +154,15 @@ HRESULT CScene_IMGUI::Ready_Layer_Terrain(list<SPECIALCUBE*>* listdata)
 		{
 			if (count_Potal % 2 == 0)
 			{
-				memcpy(potalDesc.vPos_A_Cube, &(data->WorldMat[3]), sizeof(_float3));
+
+				memcpy(&potalDesc.vPos_A_Cube, &(data->WorldMat.m[3]), sizeof(_float3));
 
 			}
 			else
 			{
-				memcpy(potalDesc.vPos_B_Cube,&(data->WorldMat[3]),sizeof(_float3));
+				memcpy(&potalDesc.vPos_B_Cube,&(data->WorldMat.m[3]),sizeof(_float3));
 				// 积己
-				GetSingle(CGameInstance)->Add_GameObject_To_Layer(potalDesc.iNowScene,
+				GetSingle(CGameInstance)->Add_GameObject_To_Layer(SCENE_IMGUISCENE,
 					TAG_LAY(Layer_Terrain), TAG_OP(Prototype_PortalCube_A),
 					&potalDesc);
 
@@ -166,23 +174,25 @@ HRESULT CScene_IMGUI::Ready_Layer_Terrain(list<SPECIALCUBE*>* listdata)
 		// 郡府海捞磐 积己规侥
 		if (data->Tagname == TAG_OP(Prototype_EscalatorCube))
 		{
+
 			if (count_Escalator % 2 == 0)
 			{
-				memcpy(potalDesc.vPos_A_Cube, &(data->WorldMat[3]), sizeof(_float3));
+				memcpy(escalDesc.vEndPos, &(data->WorldMat.m[3]), sizeof(_float3));
 
 			}
 			else
 			{
-				memcpy(potalDesc.vPos_B_Cube, &(data->WorldMat[3]), sizeof(_float3));
+				memcpy(escalDesc.vStartPos, &(data->WorldMat.m[3]), sizeof(_float3));
+				// 积己
+				GetSingle(CGameInstance)->Add_GameObject_To_Layer(SCENE_IMGUISCENE,
+					TAG_LAY(Layer_Terrain), TAG_OP(Prototype_EscalatorCube),
+					&escalDesc);
 				
 			}
 			count_Escalator++;
 
 		}
-
 	}
-
-
 	return S_OK;
 }
 
